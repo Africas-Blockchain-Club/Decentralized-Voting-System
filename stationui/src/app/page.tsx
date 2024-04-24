0c56044aabead635b128e5d99e1ffaebcf4edac1
dselokela023 <dselokela023@student.wethinkco.de>
2024-04-25 01:13:26 +0200

    Project init(backend & frontend)

---
+++ stationui/src/app/page.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <div className=" justfy-center mx-auto">
+      <div className="mockup-phone ">
+          <div className="camera"></div> 
+            <div className="display">
+              <div className="artboard artboard-demo phone-1">Decentralized Voting System</div>
+            </div>
+      </div>
+    </div>
+  );
+}
