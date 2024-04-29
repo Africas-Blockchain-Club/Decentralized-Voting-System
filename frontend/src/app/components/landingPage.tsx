fe96439641dcf7f7bd13c9bee26d58df2c7144b5
DenzelSE <125692067+DenzelSE@users.noreply.github.com>
2024-04-29 17:48:41 +0000

    connect

--- frontend/src/app/components/landingPage.tsx
+++ frontend/src/app/components/landingPage.tsx
@@ -3,12 +3,17 @@ import React, {useState} from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import Layout from "./layout";
+import Web3 from "web3";
 
-const LandingPage = () => { 
-    const path = usePathname();
-    const isLinkActive = (href: any) => {
-        return path === href;
-    };
+const LandingPage = (props: {connect: Function}) => { 
+    // const path = usePathname();
+    // const isLinkActive = (href: any) => {
+    //     return path === href;
+    // };
+
+    
+
+
 
     return (
     <Layout>
@@ -19,11 +24,11 @@ const LandingPage = () => {
                     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Welcome to CryptoVotes_</h1>
                     <p className="mb-8 text-lg font-normal text-yellow lg:text-2xl sm:px-16 xl:px-48 ">A decentralized voting app. Giving you Security and Transparency</p>
                     <div className="flex flex-col mb-8 lg:mb-16 hover:text-blue-700 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-0">
-                    <Link href="/ConnectWallet">
-                        <div className={"inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg focus:outline-none focus:ring-4 dark:focus:ring-primary-900 " + (isLinkActive("/ConnectWallet") ? "bg-gray-800 text-white hover:bg-gray-900 focus:ring-primary-300" : "bg-gray-800 text-yellow-400 hover:bg-gray-700 focus:ring-gray-100 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700")}>
-                        Connect wallet
+                    {/* <Link href="/ConnectWallet"> */}
+                        <div onClick={props.connect}  className={"inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center rounded-lg focus:outline-none focus:ring-4 dark:focus:ring-primary-900 bg-gray-800 text-yellow-400 hover:bg-gray-700 focus:ring-gray-100 dark:bg-gray-800 dark:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"}>
+                        Connect To Vote
                         </div>
-                    </Link>
+                    {/* </Link> */}
                     </div>
                 </div>
             </div>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,16 +1,46 @@
+"use client"
+import { useState } from "react";
 import BottomNav from "./components/BottomNav";
 import NavBar from "./components/NavBar";
 import LandingPage from "./components/landingPage";
+import Web3 from "web3";
+import Register from "./Register/page";
 
 
 export default function Home() {
+  const [web3, setWeb3] = useState<Web3 | null>(null);
+    const [accounts, setAccounts] = useState<string[]>([]);
+    const [contract, setContract] = useState(null);
+    const [isConnected, setIsConnect] = useState(false);
+  
+  
+    const connect = async () => {
+      if (window.ethereum) {
+        try {
+          const web3Instance = new Web3(window.ethereum);
+          setWeb3(web3Instance);
+          const accounts = await web3Instance.eth.requestAccounts();
+          setAccounts(accounts);
+
+          if (accounts.length > 0) {
+            setIsConnect(true)
+          }
+
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        console.error('Web3 not found');
+      }
+    };
+
   return (
     <div>
       <div>
         <NavBar/>
       </div>
       <div>
-        <LandingPage/>
+        {(isConnected) ? <Register /> : <LandingPage connect={connect}/>}
       </div>
     <BottomNav/>
     </div>
